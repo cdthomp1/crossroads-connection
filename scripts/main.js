23c9e429e5051be0668613f02969c0bafcffd120
cdthomp1 <tho17015@byui.edu>
2019-10-30 01:28:46 -0600

    Quick mobile test

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+$(".click").click((event) => {
+    var id = (event.target.getAttribute("id"));
+    console.log(id)
+  
+        $(".contentContainer").addClass("hidden");
+        $("#" + id  + "Container").removeClass("hidden");
+    
+  });
+
+$(".list-group-item").click((event) => {
+    var id = (event.target.getAttribute("id"));
+    $("#mainNav").addClass("hidden")
+    $("#crossRoadsContainer").addClass("hidden")
+    $(".resturantContainer").removeClass("hidden")
+    console.log(id)
+})
+
+$("#i-arrow-left").click((event) => {
+    $("#mainNav").removeClass("hidden")
+    $("#crossRoadsContainer").removeClass("hidden")
+    $(".resturantContainer").addClass("hidden")
+})
